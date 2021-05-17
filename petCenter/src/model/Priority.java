1f2190a8181ff2fd0aec88ba13c8151e84540f66
Juan Felipe Castillo Gomez <1006071586@u.icesi.edu.co>
2021-05-16 23:18:03 -0500

    Arreglando todo

--- petCenter/src/model/Priority.java
+++
@@ -1,6 +0,0 @@
-package model;
-
-public enum Priority{
-	PRIORIDAD_1, PRIORIDAD_2, PRIORIDAD_3, PRIORIDAD_4, PRIORIDAD_5 
-
-}
--- petCenter/src/model/Specie.java
+++
@@ -1,6 +0,0 @@
-package model;
-
-public enum Specie{
-	PERRO, GATO, CONEJO, REPTIL, PAJARO
-
-}
